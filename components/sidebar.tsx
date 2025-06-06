eb34c9dabf499ce07cc7bfb21a5055b96473f91f
sujan49roy <sujan48roy@gmail.com>
2025-06-06 19:55:33 +0545

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -102,11 +102,7 @@ useEffect(() => {
         )}
 
         <div className="py-4 px-4">
-          <input
-            type="search"
-            placeholder="Search blogs..."
-            className="w-full px-3 py-2 text-sm bg-gray-100 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+         
         </div>
 
         <div className="py-2 h-full">
